2419c7d2ee4e3f86c8134c51fc7147dfb95e29fc
Syphax Mokraoui <119063581+phaxypop@users.noreply.github.com>
2026-03-10 03:10:43 -0400

    ajustement de la section music et albums

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,31 @@ function moveSlide(direction) {
 
   track.style.transform = "translateX(-" + index * 100 + "%)";
 }
+//////////////////////////////////////////////////////////////////
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls (optional)
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
